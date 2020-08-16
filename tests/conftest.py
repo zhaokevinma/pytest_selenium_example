adb099ad19e62d0d1c7765252e102e63f0988afb
Kevin Malone <zhaokevinma@gmail.com>
2020-08-15 22:59:52 -0400

    Added pytest-html

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,6 @@ from pytest import fixture
 from selenium import webdriver
 
 @fixture(scope='session')
-def chrome_browser():
+def chrome_driver():
     browser = webdriver.Chrome()
     yield browser
--- tests/test_trial_page.py
+++ tests/test_trial_page.py
@@ -2,8 +2,8 @@ from pytest import mark
 from pages.trial_page import TrialPage
 
 @mark.smoke
-def test_trial_page(chrome_browser):
-    trial_page = TrialPage(driver=chrome_browser)
+def test_trial_page(chrome_driver):
+    trial_page = TrialPage(driver=chrome_driver)
     trial_page.go()
     trial_page.stone_input.input_text("rock")
     trial_page.stone_button.click()
